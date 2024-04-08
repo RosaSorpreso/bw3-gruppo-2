26a60b3351657306b4ce8013e424edd19f102773
Cristur <cristianoturri@gmail.com>
2024-04-08 16:38:03 +0200

    Restart

--- fantaepicode/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  usersUrl: 'http://localhost:3000/users',
-  playersUrl: 'http://localhost:3000/players',
-  loginUrl:"http://localhost:3000/login",
-  registerUrl:"http://localhost:3000/register"
-};
