16ee0970f41298e565c223973baf82cd63db2ec6
RosaSorpreso <rosasorpreso@gmail.com>
2024-04-08 13:00:01 +0200

    sidebar

---
+++ fantaepicode/src/app/main-components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  @Input() isExpanded: boolean = false;
+  @Output() toggleSidebar: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  handleSidebarToggle = () => this.toggleSidebar.emit(!this.isExpanded);
+}
