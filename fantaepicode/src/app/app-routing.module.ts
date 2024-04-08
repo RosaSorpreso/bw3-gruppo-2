e633a715d6d27f86821be1ef9afc70bebf7b824c
RosaSorpreso <rosasorpreso@gmail.com>
2024-04-08 11:54:25 +0200

    moduli creati

---
+++ fantaepicode/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule)
+  },
+  {
+    path: 'players',
+    loadChildren: () => import('./pages/players/players.module').then(m => m.PlayersModule)
+  },
+  {
+    path: 'create',
+    loadChildren: () => import('./backoffice/create/create.module').then(m => m.CreateModule)
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('./backoffice/edit/edit.module').then(m => m.EditModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
