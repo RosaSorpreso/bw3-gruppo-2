74b9158fbe53446787a4e20f411b07936fdca54f
RosaSorpreso <rosasorpreso@gmail.com>
2024-04-09 10:18:52 +0200

    preparazione branch

--- fantaepicode/src/app/app-routing.module.ts
+++ fantaepicode/src/app/app-routing.module.ts
@@ -8,38 +8,38 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-    canActivate: [GuestGuard]
+
   },
   {
     path: 'homepage',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
-    canActivate: [AuthGuard]
+
 
   },
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule),
-    canActivate: [AuthGuard]
+
   },
   {
     path: 'users',
     loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule),
-    canActivate: [AuthGuard]
+
   },
   {
     path: 'players',
     loadChildren: () => import('./pages/players/players.module').then(m => m.PlayersModule),
-    canActivate: [AuthGuard]
+
   },
   {
     path: 'create',
     loadChildren: () => import('./backoffice/create/create.module').then(m => m.CreateModule),
-    canActivate: [AuthGuard]
+
   },
   {
     path: 'edit',
     loadChildren: () => import('./backoffice/edit/edit.module').then(m => m.EditModule),
-    canActivate: [AuthGuard]
+
   }
 ];
 
