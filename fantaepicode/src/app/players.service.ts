4a651becc8117b2c2c986538ed0e2998e84a2cdd
OttPFC <ale.maz097@gmail.com>
2024-04-10 12:50:16 +0200

    edit fix

--- fantaepicode/src/app/players.service.ts
+++ fantaepicode/src/app/players.service.ts
@@ -1,68 +1,105 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment.development';
 import { iPlayer } from './models/player';
-import { BehaviorSubject, Observable, Observer, Subject, tap } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayersService {
 
-  constructor(private http:HttpClient) { }
+  private playerSubject = new BehaviorSubject<iPlayer[]>([]);
+  players$ = this.playerSubject.asObservable();
 
-    player = new BehaviorSubject<iPlayer[]>([]);
-    players$ = this.player.asObservable();
-    players:iPlayer[] = []
+  constructor(private http: HttpClient) { 
+    this.getAllPlayers().subscribe(
+      players => {
+        this.playerSubject.next(players);
+      }
+    );
+  }
 
-    getAllPlayers(): Observable<iPlayer[]> {
-      return this.http.get<iPlayer[]>(environment.playersUrl)
-    }
+  getAllPlayers(): Observable<iPlayer[]> {
+    return this.http.get<iPlayer[]>(environment.playersUrl)
+      .pipe(
+        catchError(error => {
+          console.error('Error fetching players:', error);
+          throw error;
+        })
+      );
+  }
 
-    fav: iPlayer[] = [];
-
-  addPlayer(player: iPlayer) {
+  addPlayer(player: iPlayer): Observable<iPlayer> {
     return this.http.post<iPlayer>(environment.playersUrl, player)
-      .subscribe(newPlayer => {
-        this.getAllPlayers();
-      });
+      .pipe(
+        tap(newPlayer => {
+          const currentPlayers = this.playerSubject.getValue();
+          this.playerSubject.next([...currentPlayers, newPlayer]);
+        }),
+        catchError(error => {
+          console.error('Error adding player:', error);
+          throw error;
+        })
+      );
   }
 
-  editPlayer(player:iPlayer){
-    return this.http.put<iPlayer>(environment.playersUrl + `/${player.id}`, player)
-    .pipe(tap((editedUser:iPlayer) => {
-      const index = this.players.findIndex(p => p.id === player.id)
-      this.players.splice(index, 1, editedUser)
-      this.player.next(this.players)
-    }))
+  editPlayer(player: iPlayer): Observable<iPlayer> {
+    return this.http.put<iPlayer>(`${environment.playersUrl}/${player.id}`, player)
+      .pipe(
+        tap(editedPlayer => {
+          const currentPlayers = this.playerSubject.getValue();
+          const index = currentPlayers.findIndex(p => p.id === player.id);
+          if (index !== -1) {
+            currentPlayers[index] = editedPlayer;
+          }
+          this.playerSubject.next([...currentPlayers]);
+        }),
+        catchError(error => {
+          console.error('Error editing player:', error);
+          throw error;
+        })
+      );
   }
 
-  deletePlayer(id: number) {
-    return this.http.delete(`${environment.playersUrl}/${id}`)
-      .subscribe(() => {
-        this.getAllPlayers();
-      });
+  deletePlayer(id: number): Observable<void> {
+    return this.http.delete<void>(`${environment.playersUrl}/${id}`)
+      .pipe(
+        tap(() => {
+          const currentPlayers = this.playerSubject.getValue();
+          const updatedPlayers = currentPlayers.filter(p => p.id !== id);
+          this.playerSubject.next(updatedPlayers);
+        }),
+        catchError(error => {
+          console.error('Error deleting player:', error);
+          throw error;
+        })
+      );
   }
 
-  addToFav(prod: iPlayer) {
-    const player = this.fav.find(player => player.id === prod.id);
-    if (!player) {
-      this.fav.push(prod);
+  fav: iPlayer[] = [];
+
+  addToFav(player: iPlayer) {
+    if (!this.isFav(player.id)) {
+      this.fav.push(player);
     }
   }
 
   removeFromFav(id: number) {
     const index = this.fav.findIndex(el => el.id === id);
-    this.fav.splice(index, 1);
+    if (index !== -1) {
+      this.fav.splice(index, 1);
+    }
   }
 
-  get favList() {
-    return new Observable((obs: Observer<iPlayer[]>) => {
+  get favList(): Observable<iPlayer[]> {
+    return new Observable((obs) => {
       obs.next(this.fav);
     });
   }
 
-  isFav(id: number) {
+  isFav(id: number): iPlayer | undefined {
     return this.fav.find(prd => prd.id === id);
   }
 }
