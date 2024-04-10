709c8dcf008d08c812e51037928fb1fec9528c07
RosaSorpreso <rosasorpreso@gmail.com>
2024-04-11 01:28:22 +0200

    favicon, nomi pagine e modifiche header

--- fantaepicode/src/app/app-routing.module.ts
+++ fantaepicode/src/app/app-routing.module.ts
@@ -8,37 +8,42 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-
+    title: 'Epicode Snap'
   },
   {
     path: 'homepage',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+    title: 'Epicode Snap | Homepage',
     canActivate: [AuthGuard]
-
   },
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule),
+    title: 'Epicode Snap | Il Tuo Profilo',
     canActivate: [AuthGuard]
   },
   {
     path: 'users',
     loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule),
+    title: 'Epicode Snap | Utenti',
     canActivate: [AuthGuard]
   },
   {
     path: 'players',
     loadChildren: () => import('./pages/players/players.module').then(m => m.PlayersModule),
+    title: 'Epicode Snap | Developers',
     canActivate: [AuthGuard]
   },
   {
     path: 'create',
     loadChildren: () => import('./backoffice/create/create.module').then(m => m.CreateModule),
+    title: 'Epicode Snap',
     canActivate: [AuthGuard]
   },
   {
     path: 'edit/:id',
     loadChildren: () => import('./backoffice/edit/edit.module').then(m => m.EditModule),
+    title: 'Epicode Snap',
     canActivate: [AuthGuard]
   }
 ];
