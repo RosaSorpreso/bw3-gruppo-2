8a08bf6579c70ba3af9d55e7a76f00003b095f7e
RosaSorpreso <rosasorpreso@gmail.com>
2024-04-11 00:37:55 +0200

    footer

--- fantaepicode/src/app/app.module.ts
+++ fantaepicode/src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './main-components/header/header.component';
-import { FooterComponent } from './main-components/footer/footer.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
