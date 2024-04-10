402f1ae5e6f6dbebcd7b92f0d868a8ec878f39b8
Cristur <cristianoturri@gmail.com>
2024-04-10 18:23:28 +0200

    home carosello

--- fantaepicode/src/app/app-routing.module.ts
+++ fantaepicode/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {
     path: 'homepage',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
-    canActivate: [AuthGuard]
+    //canActivate: [AuthGuard]
 
   },
   {
