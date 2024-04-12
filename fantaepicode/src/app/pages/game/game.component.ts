72524f4c7028f8b615e54b6c36bc8e3d7e911386
OttPFC <ale.maz097@gmail.com>
2024-04-12 16:08:15 +0200

    fix battle

--- fantaepicode/src/app/pages/game/game.component.ts
+++ fantaepicode/src/app/pages/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PlayersService } from '../../players.service';
 import { iPlayer } from '../../models/player';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-game',
@@ -11,8 +12,11 @@ export class GameComponent {
 
   players:iPlayer[] = []
   players2:iPlayer[] = []
+  strenght1:number = 0;
+  strenght2:number= 0;
+  squad:iPlayer[] = []
 
-  constructor(private playerSvc: PlayersService,){}
+  constructor(private playerSvc: PlayersService, private profile:ProfileComponent){}
   fight:boolean =false
 
 
@@ -21,13 +25,20 @@ export class GameComponent {
     this.playerSvc.getAllPlayers().subscribe(player => {
       this.players = this.shuffle(player).splice(0, 4)
       this.players2 = this.shuffle(player).splice(0, 4)
-
     });
 
     this.playerSvc.players$.subscribe(
       player => {
         this.players = player;
       });
+
+      
+
+      this.profile.squadBattle$.subscribe(player => {
+        console.log(player);
+        
+        this.squad = player
+      })
   }
   shuffle(array: any[]): any[] {
     let currentIndex = array.length;
@@ -42,5 +53,22 @@ export class GameComponent {
   }
   startFight():boolean{
     return this.fight=true
+  }
+  getNftClasses(i: number): object {
+    return {
+      ['animation-sbam' + (i + 1)]: this.fight,
+      ['animation-jump']: !this.fight
+    };
+  }
+  getNftClasses2(i: number): object {
+    return {
+      ['animation-match' + (i + 1)]: this.fight,
+      ['animation-jump']: !this.fight
+    };
+  }
+
+  battleWin(){
+    this.players.forEach(p => this.strenght1 += p.strenght)
+    this.players.forEach(p => this.strenght2 += p.strenght)
   }
 }
--- fantaepicode/src/app/pages/profile/profile.component.ts
+++ fantaepicode/src/app/pages/profile/profile.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { iUser } from '../../models/user';
 import { iPlayer } from '../../models/player';
 import { PlayersService } from '../../players.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class ProfileComponent {
+  [x: string]: any;
 
   user: iUser | undefined;
   squad!: iPlayer[]
+
+  private squadBattle = new BehaviorSubject<iPlayer[]>([]);
+  squadBattle$ = this.squadBattle.asObservable();
 
   constructor(
     private authSvc: AuthService,
@@ -28,7 +36,13 @@ export class ProfileComponent {
     })
   }
 
+  getSquad(){
+    return this.squad
+  }
+
   removePlayer(id:number){
     this.playerSvc.removeFromFav(id)
   }
+
+  
 }
